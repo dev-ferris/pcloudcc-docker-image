a489d14e394c0125237df63224b86e0465ef4350
ferris <124394208+dev-ferris@users.noreply.github.com>
2026-09-19 13:37:43 +0200

    Change default mount point to '/pcloud_internal'

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ set -eu
 : "${PCLOUD_TOTP_SECRET_FILE:=}"
 : "${PCLOUD_CRYPT:=}"
 : "${PCLOUD_CRYPT_FILE:=}"
-: "${PCLOUD_MOUNT:=/pcloud}"
+: "${PCLOUD_MOUNT:=/pcloud_internal}"
 : "${USER:=nobody}"
 : "${GROUP:=users}"
 : "${ENABLE_BINDFS:=0}"
